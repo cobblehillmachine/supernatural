2612fb614d06f900ddd8094307e83fdf414d992d
samanthavandapuye <samantha.vandapuye@gmail.com>
2014-04-17 16:58:36 -0400

    update etc

--- js/supernatural.js
+++ js/supernatural.js
@@ -1,10 +1,15 @@
 $(document).ready(function() {
-    $('#fullpage').fullpage();
-	centerItem('.inline-cont',680, 680);
+	if($(window).width() >= 981) {
+        $('#fullpage').fullpage();
+    }
+	setInputFieldFunctions();
+	showFooter();
+    
+	//centerItem('.inline-cont',680, 680);
 });
 
 $(window).resize(function() {
-	centerItem('.inline-cont',680, 680);
+	//centerItem('.inline-cont',680, 680);
 });
 
 function centerItem(item,iWidth,iHeight){  
@@ -16,4 +21,33 @@ function centerItem(item,iWidth,iHeight){
        'left': w/2,
        'top':h/2
    });   
+}
+
+function setInputFieldFunctions(){
+	$('input, textarea').each(function(){
+	    var $this = $(this);
+	    $this.data('placeholder', $this.attr('placeholder'))
+	         .focus(function(){$this.removeAttr('placeholder');})
+	         .blur(function(){$this.attr('placeholder', $this.data('placeholder'));});
+	});
+}
+
+function showFooter() {
+	$('#footer #mobile-tab.closed').on({
+		click: function() {
+		$('#footer .footer-arrow').addClass('up'); 
+		$('#footer #mobile-nav').slideToggle();
+		$(this).removeClass('closed');
+		$(this).addClass('open');
+	}
+	});
+	// $('#footer #mobile-tab.open').on({
+	// 	click: function(){
+	// 	$('#footer .footer-arrow').removeClass('up'); 
+	// 	$('#footer #mobile-nav').slideToggle();
+	// 	$(this).removeClass('open');
+	// 	$(this).addClass('closed');
+	// }
+	// });
+	
 }
