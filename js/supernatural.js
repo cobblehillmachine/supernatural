8faf625a273182c5fd2c058a012635ad4d3fd8ea
samanthavandapuye <samantha.vandapuye@gmail.com>
2014-04-23 11:38:33 -0400

    fix responsiveness

--- js/supernatural.js
+++ js/supernatural.js
@@ -1,27 +1,9 @@
 $(document).ready(function() {
-	if($(window).width() >= 981) {
-        $('#fullpage').fullpage();
-    }
+    $('#fullpage').fullpage();
 	setInputFieldFunctions();
 	showFooter();
-    
-	//centerItem('.inline-cont',680, 680);
 });
 
-$(window).resize(function() {
-	//centerItem('.inline-cont',680, 680);
-});
-
-function centerItem(item,iWidth,iHeight){  
-   windowWidth = $(window).width();
-   windowHeight = $(window).height();
-   var w = windowWidth - iWidth; 
-   var h = windowHeight - iHeight;
-   $(item).css({
-       'left': w/2,
-       'top':h/2
-   });   
-}
 
 function setInputFieldFunctions(){
 	$('input, textarea').each(function(){
